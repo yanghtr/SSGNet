421c4a75d25b389865a03d5ff161095f4938dac6
HaitaoYang <yanght@cs.utexas.edu>
2023-03-24 00:01:55 -0500

    fix bugs

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,11 +1,10 @@
 from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
 from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
 from .spconv_unet import UNetV2
-from .bev_refinement import PillarRes18BackBone8xHybridRefineBEV, PillarRes18BackBone8xRefineBEV
+from .bev_refinement import PillarRes18BackBone8xHybridRefineBEV
 
 __all__ = {
     'PillarRes18BackBone8xHybridRefineBEV': PillarRes18BackBone8xHybridRefineBEV,
-    'PillarRes18BackBone8xRefineBEV': PillarRes18BackBone8xRefineBEV,
     'VoxelBackBone8x': VoxelBackBone8x,
     'UNetV2': UNetV2,
     'PointNet2Backbone': PointNet2Backbone,
